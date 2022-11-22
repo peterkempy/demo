681b39686d0b3e4c7f56f606397b0e5ce8bb7649
kempp-student <80429852+kempp-student@users.noreply.github.com>
2022-11-22 12:34:21 +1100

    Removed getHostname

--- src/main/java/pk/demo/containers/demo/service/HostnameService.java
+++ src/main/java/pk/demo/containers/demo/service/HostnameService.java
@@ -1,23 +1,11 @@
 package pk.demo.containers.demo.service;
 
-import com.sun.jna.Library;
-import com.sun.jna.Memory;
-import com.sun.jna.Native;
-import com.sun.jna.Platform;
-import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.Kernel32Util;
 import org.json.simple.JSONObject;
 
 public class HostnameService {
 
-    private interface UnixCLibrary extends Library {
-        UnixCLibrary INSTANCE = (UnixCLibrary) Native.loadLibrary("c", UnixCLibrary.class);
-        public int gethostname(byte[] hostname, int bufferSize);
-    }
-
     public static String getMachineDetails() {
         JSONObject machineDetails = new JSONObject();
-        machineDetails.put("hostName", getHostname());
         machineDetails.put("ipAddress", getIpAddress());
         return machineDetails.toJSONString();
     }
@@ -44,23 +32,5 @@ public class HostnameService {
         return null;
 
     }
-    public static String getHostname() {
-        if (Platform.isWindows()) {
-            return Kernel32Util.getComputerName();
-        } else {
-            // For now, we'll consider anyhting other than Windows to be unix-ish enough to have gethostname
-            // TODO - Consider http://stackoverflow.com/a/10543006 as a possibly better MacOS option
 
-            byte[] hostnameBuffer = new byte[4097];
-            // http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/limits.h.html suggests
-            // the actual limit would be 255.
-
-            int result = UnixCLibrary.INSTANCE.gethostname(hostnameBuffer, hostnameBuffer.length);
-            if (result != 0) {
-                throw new RuntimeException("gethostname call failed");
-            }
-
-            return Native.toString(hostnameBuffer);
-        }
-    }
 }
